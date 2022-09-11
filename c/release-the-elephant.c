14d2b17d67274492f20019fb9f61a6dc3ca738b2
Johan de Vries <johandevries@c2-academy.com>
2022-09-11 20:00:59 +0000

    Move heredocs

---
+++ c/release-the-elephant.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int system(const char *command);
+
+int main(int argc, char *argv[])
+{ 
+   FILE *file; 
+   char str[30];
+   if (argc != 3 || strcmp(argv[1], "-l"))  {
+      printf("Usage: %s -l lockcode\n", argv[0]); 
+   } else if (strcmp(argv[2], "348731")) {
+      printf("Wrong lockcode. Please try again.\n");
+   } else {
+      system("cat /usr/share/.linux-adventures/.ascii/released-elephant; chmod 777 /mystery");      
+   }
+}
